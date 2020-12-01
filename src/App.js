436b850e0cd3000beba622f99c5b07c1575b9f8b
Karthik Sivanadiyan <ksivanad@usc.edu>
2020-11-30 20:56:06 -0800

    Completed Understanding the Base Features & Syntax

--- src/App.js
+++ src/App.js
@@ -1,31 +1,97 @@
+// React must be imported to properly work with React JSX syntax
+// Hooks contain reusable code logic that is separate from thecomponent tree. They allow us to hook up functionality to ourcomponents.
 import React, { Component } from 'react';
 import './App.css'
+// Importing CSS is okay (is not included, just to inform WebPack)
+import './Person/Person.css'
 import Person from './Person/Person'
 
+// Stateful/Smart/Container component
 class App extends Component {
+  // We just hooked this component up with state. The useState hook is a function that we can invoke to return an array. 
+  // The first value of thatarray is the state variable we want to use.
+  // The second item in the array that’s returned by the useState hook is a function that can be used to change the state value
+  // Use array destructuring to pull elements out of array on RHS
+  // When the state of a component tree changes, so do the properties. The new 
+  // data flows through the tree, causing specific leaves and branches to render 
+  // to reflect the newcontent.
   state = {
     persons: [
       { name: 'Max', age: 28 },
-      { name: 'Max', age: 29 },
-      { name: 'Max', age: 26 }
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
     ]
   }
 
-  switchNameHandler = () => {
-    console.log('Was clicked!')
+  // Best practice to append "*Handler" to methods not actively called but assigning as event handler later
+  // Note this is valid JS code (a function inside of a function), used commonly with Hooks
+  switchNameHandler = (newName) => {
+    // console.log('Was clicked!')
+    // Manipulate the state in the handler
+    // DON'T DO THIS (doesn't merge or tell react state has been updated for new rendering): this.state.persons[0].name = 'Maximilian'
+    // Use setState(...) instead
+    this.setState({ // THIS DOES NOT MERGE BUT REPLACES STATE (So use multiple useState(...) calls)
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    })
   }
 
+  nameChangedHandler = (event) => {
+    console.log(event);
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 }, // event target is the input element
+        { name: 'Stephanie', age: 27 }
+      ]
+    })
+  }
+
+  // A call to ReactDOM.render to render our component into the current DOM
   render() {
+    // Not a class property but a normal variable in a function
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        {/*DON'T add paranthesis after this.switchNameHandler because that will execute it*/}
+        <button style={style} onClick={() => this.switchNameHandler('Maximilian!!')}>Switch name</button>
+
+        {/*Reusing a component three times*/}
+
+        {/*name and age are examples of dynamic components passed in as HTML attributes */}
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+
+        {/*This element can pass content passed between opening and closing tags as a prop too */}
+        {/*We also pass a method that to dumb components that don't have direct access to state */}
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}
+          changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
+
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
       </div>
     )
   }
 }
 
 export default App;
+
+
+
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,21 @@
 import React from 'react';
-
+// Stateless/Dumb/Presentational component
+// Attributes passed into HTML component come into props (properties) argument
+// we constructed a component tree: a hierarchy ofcomponents that data was able to flow through as properties 
 const person = (props) => {
+  // We need the wrapper <div> with paranthesis to write component with
+  // multiple elements over multiple lines
+
+  
+
   return (
-    <div>
-      <p>I'm {props.name} and I am {props.age} years old</p>
-      <p style={{ color: "red" }}>{props.children}</p>
+    <div className="Person">
+      <p onClick={props.click}>I'm {props.name} and I am {props.age} years old</p>
+      {/*Note that a JS Object is passed for style attribute */}
+      {/*Note the special children property to access any elements between tags*/}
+      <p>{props.children}</p>
+      {/*Two way binding on input element */}
+      <input type="text" onChange={props.changed} value={props.name}/>
     </div>
   )
 };
